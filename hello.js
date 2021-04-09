536dc083eda1a2ba3588f13916908995f31e3f99
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-04-09 01:06:59 +0000

    Added examples to show differance of consolelog and return value

--- hello.js
+++ hello.js
@@ -5,3 +5,20 @@ const sayHello = function (name) {
 sayHello("kitty");
 sayHello("puppy");
 sayHello("squirrel")
+
+
+// this is an eample of PRINTING to the console.
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHelloToConsole('John');
+
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('jonnyboy')
+
+console.log(greeting);
